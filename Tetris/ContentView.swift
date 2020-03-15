009275f12787af3f7a2b50c9719aeec33fdc0ee0
Kaiski <kaiski.official@gmail.com>
2020-03-15 12:30:24 +0300

    Initial Commit

---
+++ Tetris/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tetris
+//
+//  Created by Динара Аминова on 15/03/2020.
+//  Copyright © 2020 AlekseyKudryashov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
